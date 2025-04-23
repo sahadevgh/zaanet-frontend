04e546eac82fb87c08cd6a03000f8f5e5b10e574
realsahabia <yakubukarim12@gmail.com>
2025-04-23 02:19:17 +0000

    fixed netlify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // tells Next.js to generate a static HTML export
 };
 
 export default nextConfig;
